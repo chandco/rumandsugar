285ff2929db63ee41d82bf7f0392949674d87c88
U-CHARLTONHOUSE\nathan.edwards <nathan.edwards@NATHANEDWARD-LT.charltonhouse.co.uk>
2014-09-11 12:03:52 +0100

    some css fixes

---
+++ lib/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+
+/* This is 'functions' as added by Nathan for the rum and sugar conversion of the Croma theme so as not to mess up the original code */
+
+
+
+require("comments-off.php");
