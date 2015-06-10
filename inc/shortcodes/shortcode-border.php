39590b6c0ab4616759bc8aa3557bed7e665d8897
root <root@dev.chand.co>
2015-06-10 10:47:17 +0100

    Added a shortcode to change title on SVG dynamically

--- inc/shortcodes/shortcode-border.php
+++ inc/shortcodes/shortcode-border.php
@@ -9,4 +9,18 @@ function border_shortcode( $atts, $content = null )
     }  
 }
 
+add_shortcode('banner-title', 'banner_title_shortcode');
+
+	function banner_title_shortcode( $atts, $content = null) {
+		$atts = shortcode_atts(
+			array(
+				'title' => 'Nibblesff'
+			), $atts);
+
+		return '
+		<div class="banner-title">
+		<h3>'. $atts['title'] . '</h3>
+		</div>';
+	}
+
 ?>
